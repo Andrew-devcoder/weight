dde824b03d545ffd2afd77b5c9a593e8af63000b
Andrew-devcoder <andrey.kovpak.01@gmail.com>
2023-03-29 23:35:54 +0300

    modul1 draft

--- src/js/app.js
+++ src/js/app.js
@@ -2,18 +2,18 @@ import * as flsFunctions from "./modules/function.js";
 
 flsFunctions.isWebp();
 
-
-
 const squatInput = document.getElementById("squat-input");
 const benchPressInput = document.getElementById("benchpress-input");
 const deadliftInput = document.getElementById("deadlift-input");
 
 document.getElementById("submit-button").addEventListener("click", function () {
-	const squat = squatInput.value;
-	const benchPress = benchPressInput.value;
-	const deadlift = deadliftInput.value;
+    const squat = squatInput.value;
+    const benchPress = benchPressInput.value;
+    const deadlift = deadliftInput.value;
 
-	console.log("Squat:", squat);
-	console.log("Bench Press:", benchPress);
-	console.log("Deadlift:", deadlift);
+    console.log("Squat:", squat);
+    console.log("Bench Press:", benchPress);
+    console.log("Deadlift:", deadlift);
+
+    window.location.assign("modul1.html");
 });
