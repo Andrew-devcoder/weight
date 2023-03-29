8bd6c53155b1f1df42cc7e9dc53b6014393f5ef0
Andrew-devcoder <andrey.kovpak.01@gmail.com>
2023-03-29 14:03:34 +0300

    setting

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,18 @@ import * as flsFunctions from "./modules/function.js";
 
 flsFunctions.isWebp();
 
-import Swiper, { Navigation, Pagination } from 'swiper';
 
-const swiper = new Swiper();
+
+const squatInput = document.getElementById("squat-input");
+const benchPressInput = document.getElementById("benchpress-input");
+const deadliftInput = document.getElementById("deadlift-input");
+
+document.getElementById("submit-button").addEventListener("click", function () {
+	const squat = squatInput.value;
+	const benchPress = benchPressInput.value;
+	const deadlift = deadliftInput.value;
+
+	console.log("Squat:", squat);
+	console.log("Bench Press:", benchPress);
+	console.log("Deadlift:", deadlift);
+});
