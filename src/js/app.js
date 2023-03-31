e60460853dc573ab2b9fcf66f40b0f2ab2ccc393
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2023-03-31 15:39:10 +0300

    fixed js for squat , press: draft

--- src/js/app.js
+++ src/js/app.js
@@ -26,19 +26,56 @@ function inputWeight() {
         });
 }
 
+// function calculateResult(maxWeight, maxWeightPercent) {
+//     let percent = maxWeightPercent / 100;
+//     let result = percent * maxWeight;
+//     let closestNumber;
+
+//     if (result % 5 === 0 || result % 2.5 === 0) {
+//         closestNumber = result;
+//     } else {
+//         let candidate1 = Math.ceil(result / 2.5) * 2.5;
+//         let candidate2 = Math.ceil(result / 5) * 5;
+
+//         closestNumber =
+//             Math.abs(candidate1 - result) <= Math.abs(candidate2 - result)
+//                 ? candidate1
+//                 : candidate2;
+//     }
+
+//     return closestNumber;
+// }
+
 function calculateResult(maxWeight, maxWeightPercent) {
     let percent = maxWeightPercent / 100;
+    let result = percent * maxWeight;
+    let closestNumber;
 
-    let result = percent * maxWeight; // Вычисляем % от максимального веса
+    if (result % 5 === 0 || result % 2.5 === 0) {
+        closestNumber = result;
+    } else {
+        let candidate1 = Math.ceil(result / 2.5) * 2.5;
+        let candidate2 = Math.floor(result / 2.5) * 2.5;
+        let candidate3 = Math.ceil(result / 5) * 5;
+        let candidate4 = Math.floor(result / 5) * 5;
 
-    let candidate1 = Math.ceil(result / 2.5) * 2.5; // Ближайшее число, которое делится на 2.5 без остатка
-    let candidate2 = Math.ceil(result / 5) * 5; // Ближайшее число, которое делится на 5 без остатка
+        let diff1 = Math.abs(result - candidate1);
+        let diff2 = Math.abs(result - candidate2);
+        let diff3 = Math.abs(result - candidate3);
+        let diff4 = Math.abs(result - candidate4);
 
-    // Выбираем число, ближайшее к результату, из двух кандидатов
-    let closestNumber =
-        Math.abs(candidate1 - result) < Math.abs(candidate2 - result)
-            ? candidate1
-            : candidate2;
+        let minDiff = Math.min(diff1, diff2, diff3, diff4);
+
+        if (minDiff === diff1) {
+            closestNumber = candidate1;
+        } else if (minDiff === diff2) {
+            closestNumber = candidate2;
+        } else if (minDiff === diff3) {
+            closestNumber = candidate3;
+        } else {
+            closestNumber = candidate4;
+        }
+    }
 
     return closestNumber;
 }
