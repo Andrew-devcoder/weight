3472c4ba0a904938ac7316211157b31889415890
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2023-05-28 17:46:46 +0300

    test

--- src/js/app.js
+++ src/js/app.js
@@ -6,9 +6,9 @@ import { moduleThirdDay } from "./modules/moduleThirdDay.js";
 
 window.addEventListener("DOMContentLoaded", () => {
     if (
-        window.location.href.includes(
-            "index.html" || "http://localhost:3000/" || "lifter"
-        )
+        window.location.href.includes("index.html") ||
+        window.location.href.includes("http://localhost:3000/") ||
+        window.location.href.includes("lifter")
     ) {
         localStorage.clear();
         inputWeight();
